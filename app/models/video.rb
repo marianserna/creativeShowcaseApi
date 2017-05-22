1a17b388cf24b8bb0b4e02b9e4190143798693b1
Marian Serna <marianhalliday@gmail.com>
2017-05-22 14:54:05 -0400

    Created Video model + routing

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,7 @@
 class Video < ApplicationRecord
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates :vimeo_id, :video_type, :image, :title, :hearts_count, presence: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :videos, only: [:index, :create] do
+    collection do
+      get :motion
+      get :vr
+      get :interactive
+      get :featured
+    end
+
+    member do
+      post :heart
+    end
+  end
 end
