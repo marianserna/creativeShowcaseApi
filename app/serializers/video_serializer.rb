9a97ec9d4767433f39dd8e2dc7b72742e0934c99
Marian Serna <marianhalliday@gmail.com>
2017-05-22 20:57:47 -0400

    configuring paperclip defaults for amazon s3

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -2,9 +2,9 @@ class VideoSerializer < ActiveModel::Serializer
   attributes :id, :vimeo_id, :video_type, :image_url, :title, :hearts_count
 
   def image_url
-    # no access to Video so, use object
     if Rails.env.development?
       host = 'http://localhost:3000'
+      # no access to Video so, use object
       host + object.image.url(:medium)
     else
       object.image.url(:medium)
